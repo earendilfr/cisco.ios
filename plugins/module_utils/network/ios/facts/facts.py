73a8f51b160741afe66381d00b9bfbece315479e
ambroise <earendil@toleressea.fr>
2023-08-12 00:09:53 +0200

    os_user_global - Replace old module ios_user
    Replace the module ios_user with a more standard file coverage.
    And also take correctly some parts of the commands (by example, the
    current module doesn't take correctly the `privilege`part of the
    username command

--- plugins/module_utils/network/ios/facts/facts.py
+++ plugins/module_utils/network/ios/facts/facts.py
@@ -96,6 +96,9 @@ from ansible_collections.cisco.ios.plugins.module_utils.network.ios.facts.snmp_s
 from ansible_collections.cisco.ios.plugins.module_utils.network.ios.facts.static_routes.static_routes import (
     Static_routesFacts,
 )
+from ansible_collections.cisco.ios.plugins.module_utils.network.ios.facts.user_global.user_global import (
+    User_globalFacts,
+)
 from ansible_collections.cisco.ios.plugins.module_utils.network.ios.facts.vlans.vlans import (
     VlansFacts,
 )
@@ -134,6 +137,7 @@ FACT_RESOURCE_SUBSETS = dict(
     vxlan_vtep=Vxlan_vtepFacts,
     evpn_global=Evpn_globalFacts,
     evpn_evi=Evpn_eviFacts,
+    user_global=User_globalFacts,
 )
 
 
